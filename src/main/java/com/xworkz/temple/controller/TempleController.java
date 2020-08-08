e8281fac7bd17af371d9e57d4d22692599bc1f93
xworkzodc <48981565+xworkzodc@users.noreply.github.com>
2020-08-08 09:18:19 +0530

    Update TempleController.java

--- src/main/java/com/xworkz/temple/controller/TempleController.java
+++ src/main/java/com/xworkz/temple/controller/TempleController.java
@@ -60,6 +60,7 @@ public class TempleController {
 	@RequestMapping(value = "/sending.do", method = RequestMethod.GET)
 	public String byLanding(Model model) {
 		try {
+			//TODO DEBUG
 			logger.info("Invoked landing Method with success.jsp");
 			logger.info("Taking the values from the list");
 			model.addAttribute("specialEntryList", entryList);
@@ -77,7 +78,7 @@ public class TempleController {
 			return "register";
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
-			e.printStackTrace();
+			
 		}
 		logger.info("Passing the Value");
 		return null;
@@ -86,6 +87,16 @@ public class TempleController {
 		
 	}
 	
+	/**
+	*
+	*	<c:if test="${dataAvailable}">
+	*	<h4>Email Address is : ${personalDTO.email}</h4>
+	*	<h4>Phone  is : ${personalDTO.phone}</h4>
+	*
+	*	</c:if>
+	*/
+	
+	
 	@RequestMapping(value = "/register.do", method = RequestMethod.POST)
 	public String bySuccess(PersonalDTO personalDTO,VisitDTO visitDTO,Model model) {
 		try {
@@ -93,42 +104,15 @@ public class TempleController {
 			int check=this.personalAndVisitService.validateAndCreate(personalDTO, visitDTO);
 			if (check==0) {
 			//personalAndVisitService.validateAndCreate(personalDTO,visitDTO);
-			logger.info("Values of PrsonalEntity"+personalDTO);
-			
-			model.addAttribute("name", "Name of the Visiter : " + personalDTO.getName());
-			logger.info("Name of the Visiter : " + personalDTO.getName());
-			model.addAttribute("mobile", "Mobile of the Visiter : " + personalDTO.getMobile());
-			logger.info("Mobile of the Visiter : " + personalDTO.getMobile());
-			model.addAttribute("state", "State of the Visiter : " + personalDTO.getState());
-			logger.info("State of the Visiter : " + personalDTO.getState());
-			model.addAttribute("address", "Address of the Visiter : " + personalDTO.getAddress());
-			logger.info("Address of the Visiter : " + personalDTO.getAddress());
-			model.addAttribute("age", "Age of the Visiter : " + personalDTO.getAge());
-			logger.info("Age of the Visiter : " + personalDTO.getAge());
-			model.addAttribute("email", "Email of the Visiter : " + personalDTO.getEmail());
-			logger.info("Email of the Visiter : " + personalDTO.getEmail());
-			
-			logger.info("Values of VisitEntity"+visitDTO);
-			
-			model.addAttribute("se", "Visiter Entry : "+visitDTO.getSpecialEntryList());
-			logger.info( "Visiter Entry : "+visitDTO.getSpecialEntryList());
-			model.addAttribute("nop", "No Of Person in the Entry : "+visitDTO.getNoOfPersonList());
-			logger.info("No Of Person in the Entry : "+visitDTO.getNoOfPersonList());
-			model.addAttribute("date", "Date of Visiting : "+visitDTO.getDate());
-			logger.info("Date of Visiting : "+visitDTO.getDate());
-			model.addAttribute("idList", "Id of Entry : "+visitDTO.getIdList());
-			logger.info("Id of Entry : "+visitDTO.getIdList());
-			model.addAttribute("idNo", "Id No of the Entry : "+visitDTO.getIdNo());
-			logger.info("Id No of the Entry : "+visitDTO.getIdNo());
-			model.addAttribute("prasasa", "Prasada Type : "+visitDTO.getPrasadaList());
-			logger.info("Prasada Type : "+visitDTO.getPrasadaList());
-			model.addAttribute("poojatype", "Pooja Type Entry : "+visitDTO.getPoojaTypeList());
-			logger.info("Pooja Type Entry : "+visitDTO.getPoojaTypeList());
-			
+			logger.info("Values of PrsonalEntity"+personalDTO);						
+			model.addAttribute("personalDTO", personalDTO);	
+			model.addAttribute("visitDTO", visitDTO);
+			model.addAttribute("dataAvailable", true);					
 			return "success";
 			//return "register";
 			}else {
 			logger.info("Data is Not Validated Back to Main Page");
+			model.addAttribute("dataAvailable", false);	
 			return "register";
 			}
 		} catch (Exception e) {
